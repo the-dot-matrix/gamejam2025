96e12aaf7c71ade05c01f7faca0910a19b21057b
the-dot-matrix <transclosure@gmail.com>
2025-05-12 15:03:59 -0400

    update config

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 _G.DEBUG = false
-_G.TICKRATE = 1 / 120
-_G.VSYNC = true
+_G.TICKRATE = 1 / 240
+_G.VSYNC = false
 fennel = require("bin.fennel")
 fennel.install({correlate=true})
 fennel.dofile("main.fnl")

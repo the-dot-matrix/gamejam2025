55a9077c2b73342b91982a45e98e3248ea6085d0
the-dot-matrix <transclosure@gmail.com>
2025-05-13 01:00:50 -0400

    basic crt done
    needs more pixel bleeding and chromatic abberation

--- img/crt.glsl
+++ img/crt.glsl
@@ -3,18 +3,21 @@ const float offsety = 1/200;
 vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
   int mx = int(mod(sxy.x, 3));
   int my = int(mod(sxy.y, 3));
-  int xx = (mx==2) ? 1 : -1;
-  int yy = (my==2) ? 1 : -1;
-  int d = mx+my;
+  int xx = (mx==0) ? 0 : (mx==2) ? 1 : -1;
+  int yy = (my==0) ? 0 : (my==2) ? 1 : -1;
+  int d = mx+my+1;
   vec2 mxy = vec2(txy.x+(xx*offsetx), txy.y+(yy*offsety));
-  vec4 average = vec4(0);
+  vec4 avg = vec4(0);
   for (int y = -1; y <= 1; y+=1) {
     for (int x = -1; x <= 1; x+=1) {
       vec2 oxy = vec2(mxy.x+(x*offsetx), mxy.y+(y*offsety));
-      average += Texel(tex, oxy);
+      avg += Texel(tex, oxy);
     }
   }
-  average = average / 3;
-  average = color * average;
-  return (mx==0 && my==0) ? average : (average/(d+1));
+  avg = vec4(avg.xyz/3, avg.w);
+  float M = (avg.x+avg.y+avg.z)/3;
+  avg = color * avg;
+  vec4 bleed = vec4(M*avg.x,M*avg.y,(M/d)*(avg.z+(d/3)),avg.w);
+  bleed = vec4(bleed.xyz/d, bleed.w);
+  return (mx==0 && my==0) ? avg : bleed;
 }

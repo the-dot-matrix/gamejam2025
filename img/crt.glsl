e50b930595c603c0aab124b729351aea8f5b78ba
the-dot-matrix <transclosure@gmail.com>
2025-05-13 02:14:18 -0400

    removed color mods
    was buggy at varying luminance, should add bloom/chromatic effects separately

--- img/crt.glsl
+++ img/crt.glsl
@@ -5,19 +5,17 @@ vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
   int my = int(mod(sxy.y, 3));
   int xx = (mx==0) ? 0 : (mx==2) ? 1 : -1;
   int yy = (my==0) ? 0 : (my==2) ? 1 : -1;
-  int d = mx+my+1;
+  int d = mx+my;
   vec2 mxy = vec2(txy.x+(xx*offsetx), txy.y+(yy*offsety));
   vec4 avg = vec4(0);
-  for (int y = -1; y <= 1; y+=1) {
-    for (int x = -1; x <= 1; x+=1) {
+  for (int y = -my; y <= my; y+=1) {
+    for (int x = -mx; x <= mx; x+=1) {
       vec2 oxy = vec2(mxy.x+(x*offsetx), mxy.y+(y*offsety));
       avg += Texel(tex, oxy);
     }
   }
-  avg = vec4(avg.xyz/3, avg.w);
-  float M = (avg.x+avg.y+avg.z)/3;
-  avg = color * avg;
-  vec4 bleed = vec4(M*avg.x,M*avg.y,(M/d)*(avg.z+(d/3)),avg.w);
-  bleed = vec4(bleed.xyz/d, bleed.w);
-  return (mx==0 && my==0) ? avg : bleed;
+  avg = vec4(avg.xyz/(((2*mx)+1)*((2*my)+1)), avg.w);
+  float M = ((avg.x+avg.y+avg.z)/3)-0.5;
+  vec4 bleed = vec4((avg.xyz+M+0.5)/(1+d-((M+0.5)*d)), avg.w);
+  return (mx==0 && my==0) ? color * avg : color * bleed;
 }

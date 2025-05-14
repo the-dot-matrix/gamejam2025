032f2b67d85f21b5403dba0c30bdd1a2fb505dd9
the-dot-matrix <transclosure@gmail.com>
2025-05-14 14:25:18 -0400

    reduce crt shader bleed

--- img/crt.glsl
+++ img/crt.glsl
@@ -9,13 +9,13 @@ vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
   vec2 srcxy = vec2(txy.x+(sx*offsetx), txy.y+(sy*offsety));
   vec4 src = Texel(tex, srcxy);
   vec4 avg = vec4(0);
-  for (int y = -2; y <= 2; y+=1) {
-    for (int x = -2; x <= 2; x+=1) {
+  for (int y = -1; y <= 1; y+=1) {
+    for (int x = -1; x <= 1; x+=1) {
       vec2 oxy = vec2(srcxy.x+(x*offsetx),srcxy.y+(y*offsety));
       avg += Texel(tex, oxy);
     }
   }
-  avg = vec4(avg.xyz/25, avg.w);
+  avg = vec4(avg.xyz/9, avg.w);
   float M = 0;
   M += max(src.x,src.y);
   M += max(src.y,src.z);

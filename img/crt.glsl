6a031e325c9dce65b64a6e4510d4cdfaaf04a9a9
the-dot-matrix <transclosure@gmail.com>
2025-05-15 00:32:32 -0400

    upped the bloom again, bright bleed is less intense for white

--- img/crt.glsl
+++ img/crt.glsl
@@ -9,19 +9,19 @@ vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
   vec2 srcxy = vec2(txy.x+(sx*offsetx), txy.y+(sy*offsety));
   vec4 src = Texel(tex, srcxy);
   vec4 avg = vec4(0);
-  for (int y = -1; y <= 1; y+=1) {
-    for (int x = -1; x <= 1; x+=1) {
+  for (int y = -2; y <= 2; y+=1) {
+    for (int x = -2; x <= 2; x+=1) {
       vec2 oxy = vec2(srcxy.x+(x*offsetx),srcxy.y+(y*offsety));
       avg += Texel(tex, oxy);
     }
   }
-  avg = vec4(avg.xyz/9, avg.w);
+  avg = vec4(avg.xyz/25, avg.w);
   float M = 0;
   M += max(src.x,src.y);
   M += max(src.y,src.z);
   M += max(src.z,src.x);
   M /= 3;
-  vec4 bleed = vec4(avg.xyz/pow(2,d*(1-M)),avg.w);
+  vec4 bleed = vec4(avg.xyz/pow(2,d*(1.1-M)),avg.w);
   vec4 rgb = vec4(M,M,M,1);
   rgb[int(mod(sxy.x,3))] = 1;
   return (mx==0 && my==0) ? src : rgb*bleed;

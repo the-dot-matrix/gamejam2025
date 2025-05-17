c45dfed9ca71a4d6c69a7d2f250afbf3cbe83a35
the-dot-matrix <transclosure@gmail.com>
2025-05-17 17:02:46 -0400

    update shader
    fix bricklay pattern but less intense

--- img/crt.glsl
+++ img/crt.glsl
@@ -3,19 +3,22 @@ const float OY = 1.0/1200;
 const int P = 3;
 const int B = 1;
 const float D = 2;
+const int S = 1;
 vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
-  int mx = int(mod(sxy.x, P));
-  int my = int(mod(sxy.y, P));
-  float b = (mod(floor(sxy.x/P),2) * P) - (P/2.0);
-  int sx = (mx==0) ? 0 : (mx==2) ? 1 : -1;
-  int sy = (my==0) ? 0 : (my==2) ? 1 : -1;
-  int d = int(abs(sx)+abs(sy));
-  vec2 srcxy = vec2(txy.x+(sx*OX), txy.y+((sy+b)*OY));
+  int x = int(sxy.x)/S;
+  int y = int(sxy.y)/S;
+  int mx = int(mod(x, P*S));
+  float b = mod(floor(x/P),2)*(P*S/2.0);
+  int my = int(mod(y+b,P*S));
+  int sx = (int(mx/S)==2) ? 1 : (int(mx/S)==1) ? -1 : 0;
+  int sy = (int(my/S)==2) ? 1 : (int(my/S)==1) ? -1 : 0;
+  float d = abs(sy); // TODO incorporate S
+  vec2 srcxy = vec2(txy.x+(sx*OX), txy.y+(sy*OY));
   vec4 src = Texel(tex, srcxy);
   vec4 avg = vec4(0);
   for (int y = -B; y <= B; y+=1) {
     for (int x = -B; x <= B; x+=1) {
-      vec2 oxy = vec2(srcxy.x+(x*OX),srcxy.y+((sy+b)*OY));
+      vec2 oxy = vec2(srcxy.x+(x*OX),srcxy.y+(y*OY));
       avg += Texel(tex, oxy);
     }
   }
@@ -24,9 +27,9 @@ vec4 effect(vec4 color, Image tex, vec2 txy, vec2 sxy) {
   M += max(src.x,src.y);
   M += max(src.y,src.z);
   M += max(src.z,src.x);
-  M /= P;
+  M /= 3;
   vec4 bleed = vec4(avg.xyz/pow(D,d*(1.1-M)),avg.w);
   vec4 rgb = vec4(M,M,M,1);
-  rgb[int(mod(sxy.x,P))] = 1;
-  return (mx==0 && my==0) ? src : rgb*bleed;
+  rgb[int(mod(x,P))] = 1;
+  return (mx==0 && my==0) ? rgb*bleed : rgb*bleed;
 }
